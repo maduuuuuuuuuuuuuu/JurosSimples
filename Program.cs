4673d0ebb79621f4522f2d17b4a8caf4cbfa05dc
madu <maduamaral2006@gmail.com>
2023-06-29 14:37:18 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿double capital;
+double taxa;
+double meses;
+double juros;
+double montante;
+
+    Console.WriteLine("");
+    Console.WriteLine("descubra o valor dos seus juros");
+    Console.WriteLine("e de sua montante aqui!");
+    Console.WriteLine("");
+    Console.WriteLine("digite seu capital (C)");
+         capital = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("digite a taxa de juros sem o");
+    Console.WriteLine("símbolo %");
+         taxa = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("digite o total de meses");
+
+         meses = Convert.ToDouble(Console.ReadLine());
+   
+        juros = capital * (taxa / 100) * meses;
+        montante = capital + juros;
+
+    Console.WriteLine("");
+    Console.WriteLine($"seu juros é de {juros} reais");
+    Console.WriteLine("");
+    Console.WriteLine($"sua montamte é de {montante} reais");
+   
+
